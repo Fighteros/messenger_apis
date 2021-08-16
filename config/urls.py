157432fadd50ace654dc21097f6e4640c5979b98
fighteros <fighteros121212@gmail.com>
2021-08-16 08:40:28 +0200

    updated settings 4

--- config/urls.py
+++ config/urls.py
@@ -16,8 +16,16 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from config import settings
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/', include('APIs.urls')),
     path('api-auth/', include('rest_framework.urls')),
 ]
+
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns = [
+        path('__debug__/', include(debug_toolbar.urls)),
+    ] + urlpatterns
