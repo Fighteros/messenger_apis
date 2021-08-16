672ec2ae8bfc9c584e9c06da2008e2de39e9dbe5
fighteros <fighteros121212@gmail.com>
2021-08-16 08:17:36 +0200

    updated settings

--- config/settings.py
+++ config/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'django-insecure-$3ccbkxvk5-*c5_b1-#p@s39=bkh-h(%+g!pi8qrv8$jn&q$0v
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['morning-caverns-61655.herokuapp.com', 'localhost', '127.0.0.1']
 
 # Application definition
 
