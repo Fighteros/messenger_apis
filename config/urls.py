6acc3533830ce2cba49021641bc0c3f90302bd41
fighteros <fighteros121212@gmail.com>
2021-08-23 07:28:24 +0200

    to prod

--- config/urls.py
+++ config/urls.py
@@ -15,11 +15,24 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
 
-from config import settings
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Messenger-apis", description="A sample API for messenger Flutter app", default_version="1.0.0",
+        terms_of_service="Don't use without permission", contact=openapi.Contact(email="am5419@fayoum.edu.eg"),
+        license=openapi.License(name="All rights are reversed by Ahmed M Abd El-Ghany")
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,)
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/', include('APIs.urls')),
     path('api-auth/', include('rest_framework.urls')),
+    path('doc/swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('doc/redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc-ui'),
 ]
